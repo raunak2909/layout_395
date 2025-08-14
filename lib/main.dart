c7af97f4146833ea6b101a621cde37a27165d696
raunakpurohit <raunak.wscubetech@gmail.com>
2025-08-14 22:54:59 +0530

    made ui responsive

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner:false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: HomePage(),
     );
   }
